e7ea3ad7730c54277cdf229441135364a87c91b5
gavin-barro <g.ab28320@gmail.com>
2024-10-15 14:47:13 -0400

    setting up some classes, nothing crazy

---
+++ src/behavior_tree.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+import py_trees as pt
+from sklearn.model_selection import train_test_split
+
+class BehaviorTree:
+
+    """
+        TODO: YAPPPPPP
+    
+        Author: Gavin Barro and Austin Earl
+        Version: 10/15/2024
+    """
+
+    def __init__(self) -> None:
+        pass
+
+
--- src/main.py
+++ src/main.py
@@ -1,3 +1,13 @@
+from behavior_tree import BehaviorTree
+
+"""
+    Driver module to be used to run our program.
+    TODO: Yap about what the program does
+
+    Author: Gavin Barro and Austin Earl
+    Version: 10/15/2024
+
+"""
 
 
 def main() -> None:
