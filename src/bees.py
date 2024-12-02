d6c915cc230fc111a89a4b38af711ef4bba39399
Gavin Barro <g.ab28320@gmail.com>
2024-12-02 14:38:40 -0500

    finish getters and setters for bees and flowers

--- src/bees.py
+++ src/bees.py
@@ -1,11 +1,14 @@
+from flowers import Flower
+from hives import Hive
+
 class Bee:
     
-    def __init__(self, age: int, species: str, chosen_flower, chosen_patch, previous_flower, destination, home_hive,
-                    collection_start_time, current_flower,  count_carry_nectar, pollen):
+    def __init__(self, age: int, species: str, chosen_flower: Flower, previous_flower: Flower, 
+                 destination: Flower, home_hive: Hive, collection_start_time: int, current_flower: Flower,  
+                 count_carry_nectar: int, pollen: int):
         self._age = age
         self._species = species
         self._chosen_flower = chosen_flower
-        self._chosen_patch = chosen_patch
         self._previous_flower = previous_flower
         self._destination = destination
         self._home_hive = home_hive
@@ -19,7 +22,7 @@ class Bee:
         return self._age
     
     @age.setter 
-    def age(self, value: int):
+    def age(self, value: int) -> None:
         self._age = value  
         
     @property
@@ -27,77 +30,69 @@ class Bee:
         return self._species
 
     @species.setter
-    def species(self, value):
+    def species(self, value: str) -> None:
         self._species = value
 
     @property
-    def chosen_flower(self):
+    def chosen_flower(self) -> Flower:
         return self._chosen_flower
 
     @chosen_flower.setter
-    def chosen_flower(self, value):
+    def chosen_flower(self, value: Flower) -> None:
         self._chosen_flower = value
 
     @property
-    def chosen_patch(self):
-        return self._chosen_patch
-
-    @chosen_patch.setter
-    def chosen_patch(self, value):
-        self._chosen_patch = value
-
-    @property
-    def previous_flower(self):
+    def previous_flower(self) -> Flower:
         return self._previous_flower
 
     @previous_flower.setter
-    def previous_flower(self, value):
+    def previous_flower(self, value: Flower) -> None:
         self._previous_flower = value
 
     @property
-    def destination(self):
+    def destination(self) -> Flower:
         return self._destination
 
     @destination.setter
-    def destination(self, value):
+    def destination(self, value: Flower) -> None:
         self._destination = value
 
     @property
-    def home_hive(self):
+    def home_hive(self) -> Hive:
         return self._home_hive
 
     @home_hive.setter
-    def home_hive(self, value):
+    def home_hive(self, value: Hive) -> None:
         self._home_hive = value
 
     @property
-    def collection_start_time(self):
+    def collection_start_time(self) -> int:
         return self._collection_start_time
 
     @collection_start_time.setter
-    def collection_start_time(self, value):
+    def collection_start_time(self, value: int) -> None:
         self._collection_start_time = value
 
     @property
-    def current_flower(self):
+    def current_flower(self) -> Flower:
         return self._current_flower
 
     @current_flower.setter
-    def current_flower(self, value):
+    def current_flower(self, value: Flower) -> None:
         self._current_flower = value
 
     @property
-    def count_carry_nectar(self):
+    def count_carry_nectar(self) -> int:
         return self._count_carry_nectar
 
     @count_carry_nectar.setter
-    def count_carry_nectar(self, value):
+    def count_carry_nectar(self, value: int) -> None:
         self._count_carry_nectar = value
 
     @property
-    def pollen(self):
+    def pollen(self) -> int:
         return self._pollen
 
     @pollen.setter
-    def pollen(self, value):
+    def pollen(self, value: int) -> None:
         self._pollen = value  
--- src/seeds.py
+++ src/seeds.py
@@ -1,6 +1,7 @@
 class Seed:
     
-    def __init__(self, age: int, species: str, lifespan, start_of_bloom, occupied: bool, nectar_regeneration, active):
+    def __init__(self, age: int, species: str, lifespan: int, start_of_bloom: int, occupied: bool,
+                 nectar_regeneration: float, active: bool):
         self._age = age
         self._species = species
         self._lifespan = lifespan
@@ -8,6 +9,62 @@ class Seed:
         self._occupied = occupied
         self._nectar_regeneration = nectar_regeneration
         self._active = active
+    
+    @property
+    def age(self) -> int:
+        return self._age
+
+    @age.setter
+    def age(self, value: int) -> None:
+        self._age = value
+
+    @property
+    def species(self) -> str:
+        return self._species
+
+    @species.setter
+    def species(self, value: str) -> None:
+        self._species = value
+
+    @property
+    def lifespan(self) -> int:
+        return self._lifespan
+
+    @lifespan.setter
+    def lifespan(self, value: int) -> None:
+        self._lifespan = value
+
+    @property
+    def start_of_bloom(self) -> int:
+        return self._start_of_bloom
+
+    @start_of_bloom.setter
+    def start_of_bloom(self, value: int) -> None:
+        self._start_of_bloom = value
+
+    @property
+    def occupied(self) -> bool:
+        return self._occupied
+
+    @occupied.setter
+    def occupied(self, value: bool) -> None:
+        self._occupied = value
+
+    @property
+    def nectar_regeneration(self) -> float:
+        return self._nectar_regeneration
+
+    @nectar_regeneration.setter
+    def nectar_regeneration(self, value: float) -> None:
+        self._nectar_regeneration = value
+
+    @property
+    def active(self) -> bool:
+        return self._active
+
+    @active.setter
+    def active(self, value: bool) -> None:
+        self._active = value
         
         
     
