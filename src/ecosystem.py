a0bb0fd0aa49756b42df8ad433c9525e50a34a23
Austin Earl <Austin@Austins-MacBook-Pro.local>
2024-12-02 15:09:35 -0500

    Update ecosystem.py

--- src/ecosystem.py
+++ src/ecosystem.py
@@ -1,4 +1,10 @@
+import random as rand
+from bees import Bee
+
+# this is for simulation 1
 class Ecosystem:
+    BEE_WAITTIME: int = 8
+    
     def __init__(self):
         self.bees = []
         self.flowers = []
@@ -7,8 +13,19 @@ class Ecosystem:
         self.iterations = 0
         self.max_iterations = 1000
     
-    def make_bees() -> None:
-        pass
+    def make_bees(self) -> None:
+        for i in range(30):
+            curr_age = 0
+            curr_species = "Bee"
+            choosen_flower = None
+            previous_flower = None
+            home_hive = self.hives[rand(0, len(self.hives) - 1)]
+            collection_start_time = 0 # most recent time the bee has collected
+            count_carry_nectar = 0
+            pollen = 0
+            bee = Bee(curr_age, curr_species, choosen_flower, previous_flower, home_hive, 
+                      collection_start_time, count_carry_nectar, pollen)
+            self.bees.append(bee)
 
     def make_flowers() -> None:
         pass
