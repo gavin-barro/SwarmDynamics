e084900fa6c981663cd4b126a2ec00e36cd27d52
Gavin Barro <g.ab28320@gmail.com>
2024-12-02 14:54:15 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -22,11 +22,20 @@ import math
 def main() -> None:
     defaults: dict = setup()
     
+    # Baseline simulation where invasive flowers don't dominate
     simulation1 = Ecosystem()
     run_simulation(simulation1)
+    
+    # # Invasive flowers dominate because their nectar rewards are higher
+    # simulation2 = Ecosystem()
+    # run_simulation(simulation2)
+    
+    # # Bees prefer invasive flowers, causing the second bee species to take over
+    # simulation3 = Ecosystem()
+    # run_simulation(simulation3)
 
 
-def run_simulation(simulation: Ecosystem):
+def run_simulation(simulation: Ecosystem) -> None:
     pass
 
 
