276bc3525fd95f9be6576646328b739247e00433
Austin Earl <Austin@Austins-MacBook-Pro.local>
2024-12-02 14:58:32 -0500

    Update ecosystem.py

--- src/ecosystem.py
+++ src/ecosystem.py
@@ -1,2 +1,23 @@
 class Ecosystem:
-    pass
+    def __init__(self):
+        self.bees = []
+        self.flowers = []
+        self.hives = []
+        self.seeds = []
+        self.iterations = 0
+        self.max_iterations = 1000
+    
+    def make_bees() -> None:
+        pass
+
+    def make_flowers() -> None:
+        pass
+
+    def make_hives() -> None:
+        pass
+
+    def make_seeds() -> None:
+        pass
+
+    def simulation() -> None:
+        pass
