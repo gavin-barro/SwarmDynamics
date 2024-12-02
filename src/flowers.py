5dc55f1b5d1c5206fa311d17a08f1f00bc6db3dd
Gavin Barro <g.ab28320@gmail.com>
2024-12-02 14:14:51 -0500

    Update flowers.py

--- src/flowers.py
+++ src/flowers.py
@@ -1,5 +1,16 @@
 class Flower:
     
-    def __init__(self, age: int, species: str):
+    def __init__(self, age: int, species: str, flower_seeds, lifespan, nectar_regeneration, start_of_bloom, 
+                 flower_nectar, occupied: bool, blocked_seeds):
         self.age = age
         self.species = species
+        self.flower_seeds = flower_seeds
+        self.lifespan = lifespan
+        self.nectar_regeneration = nectar_regeneration
+        self.start_of_bloom = start_of_bloom
+        self.flower_nectar = flower_nectar
+        self.occupied = occupied
+        self.blocked_seeds = blocked_seeds
+        
+    
+        
